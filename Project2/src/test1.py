beff17bf89f5411be0852645403241cfdd9f257f
Jalini Srisgantharajah <31306152+juppsi@users.noreply.github.com>
2019-12-10 23:01:37 +0100

    Update test1.py

--- Project2/src/test1.py
+++ Project2/src/test1.py
@@ -29,14 +29,14 @@ def logreg():
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state= 123)
 
 	#LogisticRegression_sklearn(X_train, X_test, y_train, y_test)
-	#Defininf learning rate and regularization parameter
+	#Defining learning rate and regularization parameter
 	learning_rates = np.logspace(-6,1,8)
 	lambda_values = np.logspace(-6,1,8)
 
 	iterations = 10
 
 	#performing logistic regression on credit card data
-	LogisticRegression_self(X_train, X_test, y_train, y_test, learning_rates, lambda_values, iterations)
+	LogisticRegression_self_test(X_train, X_test, y_train, y_test, learning_rates, epochs, iterations)
 
 
 	print(df)
@@ -81,134 +81,22 @@ def run_NN():
 	nn.train(iterations)
 	test_predict = nn.predict(X_test)
 
-	#print ("eta =", eta)
-	#print ("lmbd =", lmbd)
-	#print ("accuracy =", accuracy_score(y_test, test_predict))
-
-	#Confusion_Matrix(y_test, test_predict)
-
-	#print(test_predict)
-
 
 	learning_rates = np.logspace(-5,1,7)
 	lambda_values = np.logspace(-5,1,7)
-	#lambda_values= [0]
+	
 
 	#gridsearch of the model
 	NN_GRID = nn.GridSearch(X_test, X_train, y_test, y_train, lambda_values, learning_rates, iterations=200)
-	#NN_GRID= nn.GridSearch(X, y, lambda_values, learning_rates, iterations)
-
-	#best_accuracy, best_lmbd, best_eta = NN_GRID.output_values()
-
-	#print('best accuracy:', best_accuracy)
-	#print('lambda:', best_lmbd)
-	#print('Learning rate:', best_eta)
-
-	#num_splits = 5
-
-	#cv_classification(num_splits, X, y, nn, epochs = 10, eta= 0.1, lmbd =0.0, iterations=100)
-
-	"""
 	
-	epochs_ = 10
-	batch_size_ =100
-
-	accuracy_scores = np.zeros((len(learning_rates), len(lambda_values)))
-	auc_scores = np.zeros((len(learning_rates), len(lambda_values)))
-
-	for i, eta in enumerate(learning_rates):
-		for j, lmbd in enumerate(lambda_values):
-			cv_auc, cv_accuracy = cv_classification(num_splits, X, z, nn, epochs_, batch_size_, eta, lmbd)
-
-			auc_scores[i,j] = cv_auc
-			accuracy_scores[i,j] = cv_accuracy
-			#print(auc_scores[i,j])
-
-	sns.heatmap(pd.DataFrame(auc_scores),  annot= True, fmt='g')
-	plt.title('CV auc scores')
-	plt.ylim(top = 0, bottom = learning_rates)
-	plt.ylabel('Learning rate: $\\eta$')
-	plt.xlabel('Regularization Term: $\\lambda$')
-	#plt.xticks(ticks=np.arange(len(learning_rates)) + 0.5, labels=learning_rates)
-	#plt.yticks(ticks=np.arange(len(lambda_values)) + 0.5, labels=lambda_values)
-	plt.show()
-
-	sns.heatmap(pd.DataFrame(accuracy_scores),  annot= True, fmt='g')
-	plt.title('CV auc scores')
-	plt.ylim(top = 0, bottom = learning_rates)
-	plt.ylabel('Learning rate: $\\eta$')
-	plt.xlabel('Regularization Term: $\\lambda$')
-	#plt.xticks(ticks=np.arange(len(learning_rates)) + 0.5, labels=learning_rates)
-	#plt.yticks(ticks=np.arange(len(lambda_values)) + 0.5, labels=lambda_values)
-	plt.show()
-
-	"""
-
-def scikit_learn_NN():
-	"""
-
-	"""
-
-	X, y = read_dataset()
-
-
-	n_inputs = X.shape[0]
-	n_features = X.shape[1]
-
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 123)
-
-	lmbd = 0
-	eta = 0.01
-	epochs = 100
-
-	#scikit_learnNN = dnn = MLPClassifier(hidden_layer_sizes= int(n_features), activation='logistic',
-							#alpha=lmbd, learning_rate_init=eta, max_iter=epochs, random_state=1)
-	#scikit_learnNN.fit(X,y[:,0])
-	#y_predict = scikit_learnNN.predict(X)
-	#MSE =metrics.mean_squared_error(y_test, y_predict)
-	#R2_Score = regression.score(y_test, y_predict)
-	#print('MSE ={}'.format(MSE))
-	#print('R2= {}'.format(R2_Score))
-
-	#Confusion_Matrix(y[:,0], y_predict)
-
-	# store models for later use
-	eta_vals = np.logspace(-5, 1, 7)
-	lmbd_vals = np.logspace(-5, 1, 7)
-	# store the models for later use
-	DNN_scikit = np.zeros((len(eta_vals), len(lmbd_vals)), dtype=object)
-	train_accuracy = np.zeros((len(eta_vals), len(lmbd_vals)))
-	
-	for i, eta in enumerate(eta_vals):
-		for j, lmbd in enumerate(lmbd_vals):
-			dnn = MLPClassifier(hidden_layer_sizes= int(n_features), activation='logistic', 
-				alpha=lmbd, learning_rate_init=eta, max_iter=epochs, random_state=1)
-
-			dnn.fit(X_train, y_train)
-			DNN_scikit[i][j] = dnn
-			train_accuracy[i][j] = dnn.score(X_train, y_train)
-
-			test_predict = dnn.predict(X_test)
-
-			MSE =metrics.mean_squared_error(y_test, test_predict)
-			R2_Score = metrics.r2_score(y_test, test_predict)
-			print('MSE ={}'.format(MSE))
-			print('R2= {}'.format(R2_Score))
-
-	sns.set()
-	sns.heatmap(train_accuracy, annot=True, ax=ax, cmap="viridis")
-	ax.set_title("Training Accuracy")
-	ax.set_ylabel("$\eta$")
-	ax.set_xlabel("$\lambda$")
-	plt.show()
 
 
 
 
-#logreg()
+logreg()
 run_NN()
 
-#scikit_learn_NN()
+
 
 
 
